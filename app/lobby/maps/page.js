14970eec9fc0748702a381263e4461040b396013
José Consolaro <153238974+PAJEGUES@users.noreply.github.com>
2024-05-07 19:43:16 -0300

    Adicionando as atualizações do código

--- app/lobby/maps/page.js
+++ app/lobby/maps/page.js
@@ -6,6 +6,10 @@ export default function Maps(){
     const routerBack = useRouter();
 
     return(
-        <div></div>
+
+        <div class="map-responsive"><iframe src="https://www.google.com/maps/embed?q=Rua+Episcopal+numero+338+São+Carlos,+SP" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <h1>Mapas - Alef</h1>
+            <button className="btnBack" onClick={()=> routerBack.push('/lobby')}> Voltar </button>
+        </div>
     )
 }
