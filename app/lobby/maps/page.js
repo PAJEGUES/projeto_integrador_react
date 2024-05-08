d5f6eda68f5f4fd709d925abed147237f5717b3e
alef-asilva <134141941+alef-asilva@users.noreply.github.com>
2024-05-07 22:17:23 -0300

    Maps Search

--- app/lobby/maps/page.js
+++ app/lobby/maps/page.js
@@ -1,5 +1,11 @@
+import "./search"
+import Search from "./search";
+
 export default function Maps(){
     return(
-        <div class="map-responsive"><iframe src="https://www.google.com/maps/embed?q=Rua+Episcopal+numero+338+São+Carlos,+SP" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
-    )
+        <div>    
+            <Search/>
+            <div class="map-responsive"><iframe src="https://www.google.com/maps/embed?q=Rua+Episcopal+numero+338+São+Carlos,+SP" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+        </div>
+    );
 }
