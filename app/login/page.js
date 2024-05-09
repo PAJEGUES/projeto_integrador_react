d660cfd870fd4449c24e3647b4378ec2fc26f8c7
ROGER ROCHA <153238764+RogerRocha09@users.noreply.github.com>
2024-05-08 21:17:49 -0300

    login

--- app/login/page.js
+++ app/login/page.js
@@ -2,15 +2,34 @@
 import { useRouter } from 'next/navigation'
 
 export default function Login(){
-
+    <meta charset="UTF-8"></meta>
     const routerLogin = useRouter();
     const routerBack = useRouter();
+    <link rel="stylesheet" href="style.css"></link>
 
-    return(
-        <div>
-            <h1>Login - Rogério</h1>
-            <button className="btnLogin" onClick={()=> routerLogin.push('/lobby')}> Entrar </button>
+return(
+    <div class="main-login">
+        <div class="left-login">
+            <h1>Faça Login<br/>E entre para o nosso time de SEGURANÇA</h1>
+        </div>
+        <div class="right-login">
+            <div class="card-login">
+                <div class='textfield'>
+                    <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="Usuário"></input>
+                </div>
+               
+                <div class='textfield'>
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="Senha"></input>
+                </div>
+            </div>
+
+
+            <button className="btnLogin" onClick={()=> routerLogin.push('/lobby')}> Login </button>
             <button className="btnBack" onClick={()=> routerBack.push('/')}> Página Inicial </button>
         </div>
+    </div>
+    
     )
 }
