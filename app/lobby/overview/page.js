fc351dec9052e101c68f81da04aa0a9b4c472229
AilsonDeLima <ailson.plima@senacsp.edu.br>
2024-05-28 20:33:39 -0300

    ailson

--- app/lobby/overview/page.js
+++ app/lobby/overview/page.js
@@ -9,14 +9,10 @@ export default function Overview(){
     const routerBack = useRouter();
 
     const [editando, setEditando] = useState(false);
+    const [clienteEditando, setClienteEditando] = useState(null);
 
     const [filtrar, setFiltrar] = useState(false);
     const [expandir, setExpand] = useState(null);
-    const [nomeCliente, alteraNomeCliente] = useState("");
-    const [rua, alteraRua] = useState("");
-    const [bairro, alteraBairro] = useState("");
-    const [diaPagamento, alteraDiaPagamento] = useState("");
-    const [statusPagamento, alteraStatusPagamento] = useState("");
     const [clientes, setClient] = useState ([]);
 
     function getClient(){
@@ -47,7 +43,25 @@ export default function Overview(){
         })   
     }
 
-    
+    function salvarClienteEditado(id) {
+        axios.put("/api/put_client/" + id, {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(function(response) {
+            alert("Cliente editado com sucesso!");
+            getClient();
+            setEditando(false);
+            setClienteEditando(null);
+        })
+    }
+
+    function cancelarEdicao() {
+        setEditando(false);
+        setClienteEditando(null);
+        getClient(); 
+    }
 
     useEffect( () => {
         getClient();
@@ -90,89 +104,166 @@ export default function Overview(){
             
             
             <table className='card'>
+                {clientes.map(cliente => (
+                    <>
+                        <tr>
+                            <td colSpan={2}>
+                                {editando && clienteEditando === cliente.id ? (
+                                    <input 
+                                        type="text" 
+                                        value={cliente.name} 
+                                        onChange={(e) => setClient(clientes.map(c => c.id === cliente.id ? { ...c, name: e.target.value } : c))}
+                                    />
+                                ) : (
+                                    cliente.name
+                                )}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Rua</th>
+                            <td colSpan={2}>
+                                {editando && clienteEditando === cliente.id ? (
+                                    <input 
+                                        type="text" 
+                                        value={cliente.address} 
+                                        onChange={(e) => setClient(clientes.map(c => c.id === cliente.id ? { ...c, address: e.target.value } : c))}
+                                    />
+                                ) : (
+                                    cliente.address
+                                )}
+                            </td>
+                        </tr>
 
-                {
-                clientes.map( c => {
-                    return <>
-                             <tr>
-                                <td colSpan={2} > { editando ? <input value={c.name}/> : <p>{c.name}</p>} </td>
-                            </tr>
-                            
+                        {expandir !== cliente.id &&
                             <tr>
-                                <th>Rua</th>
-                                <td colSpan={2}>{c.address}</td>
+                                <td colSpan="2"><button onClick={() => setExpand(cliente.id)}>Expandir</button></td>
                             </tr>
+                        }
 
-                            {
-                                expandir !== c.id &&
-                                <tr >
-                                    <td colspan="2"><button onClick={()=> setExpand (c.id)}>Expandir</button></td>
-                                </tr>
-                            }
-
-                            {
-                            expandir === c.id &&
-
+                        {expandir === cliente.id &&
                             <>
                                 <tr>
                                     <th>N°</th>
-                                    <td>{c.housenumber}</td>
+                                    <td>
+                                        {editando && clienteEditando === cliente.id ? (
+                                            <input 
+                                                type="text" 
+                                                value={cliente.housenumber} 
+                                                onChange={(e) => setClient(clientes.map(c => c.id === cliente.id ? { ...c, housenumber: e.target.value } : c))}
+                                            />
+                                        ) : (
+                                            cliente.housenumber
+                                        )}
+                                    </td>
                                 </tr>
-                                
                                 <tr>
                                     <th>Bairro</th>
-                                    <td>{c.neighborhood}</td>
+                                    <td>
+                                        {editando && clienteEditando === cliente.id ? (
+                                            <input 
+                                                type="text" 
+                                                value={cliente.neighborhood} 
+                                                onChange={(e) => setClient(clientes.map(c => c.id === cliente.id ? { ...c, neighborhood: e.target.value } : c))}
+                                            />
+                                        ) : (
+                                            cliente.neighborhood
+                                        )}
+                                    </td>
                                 </tr>
-                                
                                 <tr>
                                     <th>Contato</th>
-                                    <td>{c.telephone}</td>
+                                    <td>
+                                        {editando && clienteEditando === cliente.id ? (
+                                            <input 
+                                                type="text" 
+                                                value={cliente.telephone} 
+                                                onChange={(e) => setClient(clientes.map(c => c.id === cliente.id ? { ...c, telephone: e.target.value } : c))}
+                                            />
+                                        ) : (
+                                            cliente.telephone
+                                        )}
+                                    </td>
                                 </tr>
-                                
                                 <tr>
                                     <th>Valor do pagamento</th>
-                                    <td>{c.paymentamount}</td>
+                                    <td>
+                                        {editando && clienteEditando === cliente.id ? (
+                                            <input 
+                                                type="text" 
+                                                value={cliente.paymentamount} 
+                                                onChange={(e) => setClient(clientes.map(c => c.id === cliente.id ? { ...c, paymentamount: e.target.value } : c))}
+                                            />
+                                        ) : (
+                                            cliente.paymentamount
+                                        )}
+                                    </td>
                                 </tr>
-                                
                                 <tr>
                                     <th>Dia do pagamento</th>
-                                    <td>{c.dateofpayment}</td>
+                                    <td>
+                                        {editando && clienteEditando === cliente.id ? (
+                                            <input 
+                                                type="text" 
+                                                value={cliente.dateofpayment} 
+                                                onChange={(e) => setClient(clientes.map(c => c.id === cliente.id ? { ...c, dateofpayment: e.target.value } : c))}
+                                            />
+                                        ) : (
+                                            cliente.dateofpayment
+                                        )}
+                                    </td>
                                 </tr>
-                                
                                 <tr>
                                     <th>Forma do pagamento</th>
-                                    <td>{c.formofpayment}</td>
-                                </tr>
-                                
-                                <tr>
-                                    <td colspan="2"><button>Confirmar pagamento</button></td>
-                                </tr>
-                                
-                                <tr>
-                                    <td colspan="2"><button>Enviar comprovante</button></td>
-                                </tr>
-                                
-                                <tr>
-                                    <td colspan="2"><button>Imprimir comprovante</button></td>
+                                    <td>
+                                        {editando && clienteEditando === cliente.id ? (
+                                            <input 
+                                                type="text" 
+                                                value={cliente.formofpayment} 
+                                                onChange={(e) => setClient(clientes.map(c => c.id === cliente.id ? { ...c, formofpayment: e.target.value } : c))}
+                                            />
+                                        ) : (
+                                            cliente.formofpayment
+                                        )}
+                                    </td>
                                 </tr>
 
-                                <tr>
-                                    <td colspan="2"><button onClick={() => setEditando (true)}>Editar</button></td>
-                                </tr>
+                                {!editando && (
+                                    <>
+                                        <tr>
+                                            <td colSpan="2"><button>Confirmar pagamento</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan="2"><button>Enviar comprovante</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan="2"><button>Imprimir comprovante</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan="2"><button onClick={() => { setEditando(true); setClienteEditando(cliente.id); }}>Editar</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan="2"><button onClick={() => deleteClient(cliente.id)}>Excluir</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan="2"><button onClick={() => setExpand(null)}>Ocultar</button></td>
+                                        </tr>
+                                    </>
+                                )}
 
-                                <tr>
-                                    <td colspan="2"><button onClick={() => deleteClient (c.id)}>Excluir</button></td>
-                                </tr>
-                                
-                                <tr>
-                                    <td colspan="2"><button onClick={()=> setExpand (null)}>Ocultar</button></td>
-                                </tr> 
+                                {editando && clienteEditando === cliente.id && (
+                                    <>
+                                        <tr>
+                                            <td colSpan="2"><button onClick={() => salvarClienteEditado(cliente.id)}>Salvar</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan="2"><button onClick={cancelarEdicao}>Cancelar</button></td>
+                                        </tr>
+                                    </>
+                                )}
                             </>
                         }
-                        </>
-                    
-                    })
-                }   
+                    </>
+                ))}
             </table>
             
         </div>
