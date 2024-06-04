b08e63b512ea39f6aa51a04fd5e1a6f03f574d5a
José Consolaro <153238974+PAJEGUES@users.noreply.github.com>
2024-06-04 19:28:59 -0300

    Arrumando Página de Login

--- app/login/page.js
+++ app/login/page.js
@@ -1,72 +1,66 @@
-'use client'
-import './style.css'
-import { useRouter } from 'next/navigation'
-import axios from "axios"
-import { useState,useEffect } from "react";
+'use client'; // Adicione esta linha no início do arquivo
 
+import './style.css';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
+export default function Login() {
+    const [email, alteraEmail] = useState("");
+    const [senha, alteraSenha] = useState("");
 
-export default function Login(){
-    const[email, alteraEmail] = useState("")
-    const[senha, alteraSenha] = useState("")
-
-    function autenticaUsuario(evento){
-        if(evento)
-            evento.preventDefault()
-
-      const usuario={
-        email:email,
-        password:senha
-      }  
-
-      axios.post("/api/login", usuario, {
-        headers: {
-            "Content-Type": "application/json",
-            "Token": "rogerio"
+    function autenticaUsuario(evento) {
+        if (evento) {
+            evento.preventDefault();
         }
-        })
-        .then((response) => {
-            console.log(response);
-            alert("Usuario autenticado com sucesso!")
-            routerPainel.push("/lobby")
-        })
-        .catch ((response) => {
-            alert("Email ou Senha incorretos...")
-        })
 
+        const usuario = {
+            email: email,
+            password: senha
+        };
+
+        axios.post("/api/login", usuario, {
+            headers: {
+                "Content-Type": "application/json",
+                "Token": "rogerio"
+            }
+        })
+            .then((response) => {
+                console.log(response);
+                alert("Usuario autenticado com sucesso!");
+                routerPainel.push("/lobby");
+            })
+            .catch((response) => {
+                alert("Email ou Senha incorretos...");
+            });
     }
 
     const routerBack = useRouter();
     const routerPainel = useRouter();
-    
 
-return(
-    <div class="main-login">
-        <div class="left-login">
-            <h1 >Faça Login<br/>E entre para o nosso time de SEGURANÇA</h1>
-            <img src="security-on-animate.svg" class="left-login-image" alt="night guard animação"></img>
-        </div>
-        <form class="right-login" onSubmit={(evento)=> autenticaUsuario(evento)}>
-            <div class="card-login">
-                <div class='textfield'>
-                    <label for="usuario">Email: </label>
-                    <input onChange={(evento)=>alteraEmail(evento.target.value)} type="text" name="usuario" placeholder=""></input>
-                </div>
-               
-                <div class='textfield'>
-                    <label for="senha">Senha: </label>
-                    <input onChange={(evento)=>alteraSenha(evento.target.value)} type="password" name="senha" placeholder=""></input>
-                </div>
+    return (
+        <div className="main-login">
+            <div className="left-login">
+                <img src="security-on-animate.svg" className="left-login-image" alt="night guard animação" draggable="false"></img>
             </div>
+            <form className="right-login" onSubmit={(evento) => autenticaUsuario(evento)}>
+                <div className="card-login">
+                    <div className='textfield'>
+                        <label htmlFor="usuario">Email: </label>
+                        <input onChange={(evento) => alteraEmail(evento.target.value)} type="text" name="usuario" placeholder=""></input>
+                    </div>
 
-            <button class="btnInvisivel"> Login </button>  
+                    <div className='textfield'>
+                        <label htmlFor="senha">Senha: </label>
+                        <input onChange={(evento) => alteraSenha(evento.target.value)} type="password" name="senha" placeholder=""></input>
+                    </div>
 
-        </form>
-        <div class="button">
-            <button class="btnLogin" onClick={()=> autenticaUsuario()}> Login </button>            
-            <button class="btnBack" onClick={()=> routerBack.push('/')}> Página Inicial </button>
+                    <div className="button-group">
+                        <button className="btn-login" onClick={() => autenticaUsuario()}>Login</button>
+                        <button className="btn-back" type="reset"> Limpar </button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </div>
-    
-    )
+    );
 }
