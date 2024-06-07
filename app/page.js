197c452d23c1cffa55531f88b62c7f85d949bfc1
José Consolaro <153238974+PAJEGUES@users.noreply.github.com>
2024-06-06 21:54:56 -0300

    Alterações para Mobile

--- app/page.js
+++ app/page.js
@@ -139,10 +139,7 @@ export default function Marketing() {
                             <option value="Sao Carlos">São Carlos</option>
                         </select>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="motocicleta">Motocicleta</label>
-                        <input type="text" id="motocicleta" name="motocicleta" required />
-                    </div>
+
                     <button type="submit" className="btn btn-2">Enviar</button>
                 </form>
                 <iframe className="map-frame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3698.6483072294013!2d-47.89397872384746!3d-22.024782579885667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b87722afe006bb%3A0x4a8b254e7543696!2sSenac%20S%C3%A3o%20Carlos!5e0!3m2!1sen!2sbr!4v1715387405233!5m2!1sen!2sbr" width="600" height="450" loading="lazy"></iframe>
